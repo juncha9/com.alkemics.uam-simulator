2742d9e6b256c064c6d3131253b41dca48af99b1
Anthony Jang <juncha9@gmail.com>
2021-12-18 13:30:56 +0900

    Update

---
+++ Assets/UAM/Scriptable/Selector.cs
@@ -0,0 +1,31 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+
+namespace Alkemic.UAM
+{
+    [Serializable]
+    public class Selector<T> where T : IKeyContainer<string>
+    {
+        [ValueDropdown("@")]
+        private string key;
+
+        [ShowInInspector]
+        private T target;
+         
+        private IEnumerable<T> items = null;
+
+        public Selector()
+        {
+
+        }
+
+        public Selector(IEnumerable<T> items)
+        {
+
+        }
+    }
+
+
+}
+
--- Assets/UAM/Scriptable/SimulateScriptable.cs
+++ Assets/UAM/Scriptable/SimulateScriptable.cs
@@ -1,19 +1,62 @@
 ﻿using Alkemic.Collections;
 using Alkemic.Scriptables;
+using JetBrains.Annotations;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Alkemic.UAM
 {
+
+
+
     [ShowOdinSerializedPropertiesInInspector]
     public class SimulateScriptable : Scriptable
     {
+
+        private string selectSimulateKey;
+
+        [ValueDropdown("@SimulateDatas")]
+        [ShowInInspector]
+        private SimulateData selectSimulate
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(selectSimulateKey) == true)
+                {
+                    return null;
+                }
+                else if (simulateDatas.ContainsKey(selectSimulateKey) == true)
+                {
+                    return null;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        
+        [HideInInspector]
         [OdinSerialize, NonSerialized]
-        private Dictionary<string, SimulateData> simulateDatas = new Dictionary<string, SimulateData>();
-        public Dictionary<string, SimulateData> SimulateDatas => simulateDatas;
+        private InnerKeyList<string, SimulateData> simulateDatas = new InnerKeyList<string, SimulateData>();
+        public InnerKeyList<string, SimulateData> SimulateDatas => simulateDatas;
+
+
+        [Button]
+        public void AddSimulate(string key)
+        {
+            if(simulateDatas.ContainsKey(key) == true)
+            {
+                Debug.LogError($"[{nameof(SimulateScriptable)}] SimulateData[{key}] is already exist");
+                return;
+            }
+        }
+
+
 
 
     }
